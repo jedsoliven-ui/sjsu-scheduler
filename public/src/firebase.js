b9b9cf7cca0bb66c3530348261611ea6803954ef
jedsoliven-ui <jed.soliven@sjsu.edu>
2026-02-06 14:47:06 -0800

    Delete public/src directory

--- public/src/firebase.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "./firebaseConfig";
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
