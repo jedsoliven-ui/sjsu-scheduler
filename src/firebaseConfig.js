b15348fbc704fa56f56a5c8ab027db94703e14a1
jedsoliven-ui <jed.soliven@sjsu.edu>
2026-02-06 15:15:24 -0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyAmJPOZUrqyDumFcisfGUaiEKy6wpz37I",
+  authDomain: "sjsu-scheduler.firebaseapp.com",
+  projectId: "sjsu-scheduler",
+  storageBucket: "sjsu-scheduler.firebasestorage.app",
+  messagingSenderId: "902250167877",
+  appId: "1:902250167877:web:2fea1a07be59053b6d2d2c"
 };
+
